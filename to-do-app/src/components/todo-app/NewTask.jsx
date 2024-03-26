08451252ce586314c2c9def34b4e6cbd77b35af7
Carlos Hernandez <carloshinzunza2@gmail.com>
2024-03-26 13:09:19 -0600

    fixed input bug on newTask component

--- to-do-app/src/components/todo-app/NewTask.jsx
+++ to-do-app/src/components/todo-app/NewTask.jsx
@@ -4,12 +4,13 @@ export default function NewTask({onAdd}){
     const [enteredTask, setEnteredTask] = useState('');
 
     function handleChange(event){
-        if(enteredTask.trim()===''){
-            return;
-        }
+        
         setEnteredTask(event.target.value)
     }
     function handleClick(){
+        if(enteredTask.trim()===''){
+            return;
+        }
         onAdd(enteredTask);
         setEnteredTask('');
     }
